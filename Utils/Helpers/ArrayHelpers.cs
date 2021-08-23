2bdfdd62f24772eab7e3b86af39415e6a3d5290b
gusgalperin <gusgalper@gmail.com>
2021-08-23 12:15:12 -0300

    first commit

---
+++ Utils/Helpers/ArrayHelpers.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections.Generic;
+
+namespace Utils
+{
+    public static class ArrayHelpers
+    {
+        /*
+         * Recibe dos arrays, ambos con datos de tipo numérico,ambos ordenados en forma ascendente,
+         * y sin repetidos dentro de cada array (puede haber repetidos entre un array y otro).
+         * Devuelve un nuevo array, que contiene todos los datos de ambos arrays, también ordenados en forma ascendente,
+         * y también sin repetidos
+         *
+         * Ejemplo
+         * Input: array1 = [1, 5, 8, 10], array2 = [2, 3, 8, 11]
+         * Output:[1, 2, 3, 5, 8, 10, 11]
+         *
+         * Observación
+         * Si se te ocurrió una solución que requiere poder ordenar un array o usar funciones de LINQ o similares,
+         * pensá en otra forma de hacerlo
+         */
+
+        public static int[] CombinarDosArrays(int[] first, int[] second)
+        {
+            var resultado = new List<int>();
+
+            return resultado.ToArray();
+        }
+
+        /*
+         * Igual que la función anterior, solo que ésta recibe un array de arrays de números ordenados en forma ascendente y sin repetidos,
+         * y devuelve un nuevo array, con la combinación de todos los números de todos los arrays recibidos,
+         * también ordenados en forma ascendente, y también sin repetidos.
+         *
+         * Ejemplo
+         * Input: arrays = [[1, 3, 10],[2, 3, 15, 16],[4],[6, 7, 13]]
+         * Output:[1, 2, 3, 4, 6, 7, 10, 13, 15, 16]
+         *
+         * Nota
+         * Asumir que solo habra valores en las posiciones [0][0...n]
+         */
+
+        public static int[] CombinarNArrays(int[][] arrays)
+        {
+            var resultado = new List<int>();
+
+            return resultado.ToArray();
+        }
+    }
+}
---
+++ tests/Utils.Tests/Helpers/ArrayHelpersTests.cs
@@ -0,0 +1,100 @@
+﻿using FluentAssertions;
+using System;
+using Xunit;
+
+namespace Utils.Tests.Helpers
+{
+    public class ArrayHelpersTests
+    {
+        [Fact]
+        public void CombinarDosArrays_AmbosNull_DevuelveArrayVacio()
+        {
+            //arrange
+            //act
+
+            var resultado = ArrayHelpers.CombinarDosArrays(null, null);
+
+            //assert
+
+            resultado.Should().BeEquivalentTo(Array.Empty<int>());
+        }
+
+        [Fact]
+        public void CombinarDosArrays_SegundoNull_DevuelveValoresDelPrimero()
+        {
+            //arrange
+            //act
+
+            var resultado = ArrayHelpers.CombinarDosArrays(new int[] { 1, 3, 8 }, null);
+
+            //assert
+
+            resultado.Should().BeEquivalentTo(new int[] { 1, 3, 8 });
+        }
+
+        [Fact]
+        public void CombinarDosArrays_PrimeroNull_DevuelveValoresDelPrimero()
+        {
+            //arrange
+            //act
+
+            var resultado = ArrayHelpers.CombinarDosArrays(null, new int[] { 1, 3, 8 });
+
+            //assert
+
+            resultado.Should().BeEquivalentTo(new int[] { 1, 3, 8 });
+        }
+
+        [Fact]
+        public void CombinarDosArrays_SinRepetidos_DevuelveValores()
+        {
+            //arrange
+            //act
+
+            var resultado = ArrayHelpers.CombinarDosArrays(new int[] { 2, 4, 11 }, new int[] { 1, 3, 8 });
+
+            //assert
+
+            resultado.Should().BeEquivalentTo(new int[] { 1, 2, 3, 4, 8, 11 });
+        }
+
+        [Fact]
+        public void CombinarDosArrays_ConRepetidos_DevuelveValores()
+        {
+            //arrange
+            //act
+
+            var resultado = ArrayHelpers.CombinarDosArrays(new int[] { 2, 4, 11 }, new int[] { 4, 3, 8 });
+
+            //assert
+
+            resultado.Should().BeEquivalentTo(new int[] { 2, 3, 4, 8, 11 });
+        }
+
+        [Fact]
+        public void CombinarDosArrays_PrimeroMasLargo_DevuelveValores()
+        {
+            //arrange
+            //act
+
+            var resultado = ArrayHelpers.CombinarDosArrays(new int[] { 2, 4, 7, 8, 9, 11, 15 }, new int[] { 4, 3, 8 });
+
+            //assert
+
+            resultado.Should().BeEquivalentTo(new int[] { 2, 3, 4, 7, 8, 9, 11, 15 });
+        }
+
+        [Fact]
+        public void CombinarDosArrays_SegundoMasLargo_DevuelveValores()
+        {
+            //arrange
+            //act
+
+            var resultado = ArrayHelpers.CombinarDosArrays(new int[] { 4, 3, 8 }, new int[] { 2, 4, 7, 8, 9, 11, 15 });
+
+            //assert
+
+            resultado.Should().BeEquivalentTo(new int[] { 2, 3, 4, 7, 8, 9, 11, 15 });
+        }
+    }
+}
